9f8e4e0a8a0bc8271685f92f5b231834b1b69521
Blayne Marjama <blayne.marjama@gmail.com>
2017-04-05 15:24:52 -0700

    initialize repo, included header rewrite

---
+++ README.md
@@ -0,0 +1,9 @@
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+# Creating a Shop-App
+Creating a shop similar to the polymer-shop-app, but with React. Using it's details/pictures/layout, but coding it on my own.
+
+### Comments
+Though it's currently being worked on, I am constantly pushing updates with the help of surge.sh. You can see the most recent version of the site [here](http://blaynestoretest.surge.sh/)
+
+Also feel free to checkout my portfolio page [here](http://blaynemarjama.surge.sh/)
---
+++ src/data/categories.js
@@ -0,0 +1,36 @@
+const categories = [
+   {
+      code: "mens_outerwear",
+      id: 1,
+      title: "Men's Outerwears",
+      img_src: "mens_outerwear",
+      img_alt: "",
+      size:"col-xs-12"
+   },
+   {
+      code: "ladies_outerwear",
+      id: 2,
+      title: "Ladies Outerwears",
+      img_src: "ladies_outerwear",
+      img_alt: "",
+      size:"col-xs-12"
+   },
+   {
+      code: "mens_tshirts",
+      id: 3,
+      title: "Men's T-Shirts",
+      img_src: "mens_tshirts",
+      img_alt: "",
+      size:"col-xs-6"
+   },
+   {
+      code: "ladies_tshirts",
+      id: 4,
+      title: "Ladies T-Shirts",
+      img_src: "ladies_tshirts",
+      img_alt: "",
+      size:"col-xs-6"
+   }
+];
+
+export default categories;
